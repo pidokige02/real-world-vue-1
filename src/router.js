319857f0bb3d2d39acf6778d84b882486b28d24d
pidokige02 <pidokige0204@gmail.com>
2022-01-08 10:18:37 +0900

    test commit from lesson4

--- src/router.js
+++ src/router.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import Router from 'vue-router' // Include the Vue Router library
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 
-Vue.use(Router)
+Vue.use(Router) //Use the Router
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/', //The URL
+      name: 'home',//Name of the route
+      component: Home //Which component to render
     },
     {
       path: '/about',
